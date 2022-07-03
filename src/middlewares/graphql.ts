ae8bf8717f25ca18cd7358b0ed6fec09247fc8ba
K.Himeno <6715229+Himenon@users.noreply.github.com>
2022-07-03 21:24:44 +0900

    chore: update

--- src/middlewares/graphql.ts
+++ src/middlewares/graphql.ts
@@ -3,13 +3,29 @@ import { buildSchema } from "graphql";
 
 const schema = buildSchema(`
 type Query {
-  hello: String
+  quoteOfTheDay: String
+  random: Float!
+  rollThreeDice: [Int]
+  rollDice(numDice: Int!, numSides: Int): [Int]
 }
 `);
 
 const rootValue = {
-  hello: () => {
-    return "Hello world!";
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within";
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+  },
+  rollDice: (args: { numDice: number; numSides?: number }) => {
+    const output = [];
+    for (let i = 0; i < args.numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (args.numSides || 6)));
+    }
+    return output;
   },
 };
 
